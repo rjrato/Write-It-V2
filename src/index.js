4a8a3feb705ea2c33994f0337cd15b1e7270c9b4
Ricardo Rato <ricardo.jorge.rato@gmail.com>
2023-10-20 13:55:48 +0100

    update files

--- src/index.js
+++ src/index.js
@@ -2,8 +2,5 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/App";
-import { Amplify } from 'aws-amplify';
-import awsExports from './aws-exports';
-Amplify.configure(awsExports);
 
 ReactDOM.render(<App />, document.getElementById("root"));
